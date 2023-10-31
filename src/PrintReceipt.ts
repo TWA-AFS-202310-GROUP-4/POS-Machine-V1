fa8e1e5dc102c678a70d96d80efbc5475c7f0599
Xinwen Xiang <XXiang2@slb.com>
2023-10-31 21:55:54 +0800

    feat: add render recepit

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -7,15 +7,17 @@ import { ReceiptItem } from './model/receipt'
 import { QuantityInfo } from './model/quantityInfo'
 
 export class PrintReceipt {
-  public printReceipt(productList: string[]):string|null {
+  public printReceipt(productList: string[]): string | null {
 
     const parsedInfo = this.aggregateProductInfo(productList)
     if (parsedInfo === null)
       return null
     //console.log(parsedInfo)
-    const receipt=this.caculateProductDetail(parsedInfo)
+    const receipt = this.caculateProductDetail(parsedInfo)
     console.log(receipt)
-    return "ssss"
+    const totalExpense=this.caculateProductsTotalExpense(receipt)
+    const totalDiscount=this.caculateProductsTotalExpense(receipt)
+    return this.renderRecepit(totalExpense,totalDiscount,receipt)
   }
 
   private isProductInfoValid(itemList: ItemInfo[], productionList: string[]): boolean {
@@ -57,45 +59,69 @@ export class PrintReceipt {
     return Number(productItem.slice(productItem.indexOf('-') + 1))
   }
 
-  private caculateProductDetail(parsedInfoList:ParsedInfo[]):ReceiptItem[]{
+  private caculateProductDetail(parsedInfoList: ParsedInfo[]): ReceiptItem[] {
     const itemList = loadAllItems()
-    const promoteInfo=loadPromotions()[0]
-    const receiptList:ReceiptItem[]=[]
-    for(let i=0;i<parsedInfoList.length;i++){
-      receiptList.push(this.caculateProductItem(itemList,promoteInfo,parsedInfoList[i]))
+    const promoteInfo = loadPromotions()[0]
+    const receiptList: ReceiptItem[] = []
+    for (let i = 0; i < parsedInfoList.length; i++) {
+      receiptList.push(this.caculateProductItem(itemList, promoteInfo, parsedInfoList[i]))
     }
     return receiptList
   }
 
-  private caculateProductItem(items:ItemInfo[],promotionInfo:PromotionInfo,parsedItem:ParsedInfo):ReceiptItem{
-    const index=items.findIndex(item=>item.barcode===parsedItem.barcode)
-    const quantityInfo:QuantityInfo={value:parsedItem.quantity,quantifier: items[index].unit+'s'}
-    const receiptItem:ReceiptItem={
+  private caculateProductItem(items: ItemInfo[], promotionInfo: PromotionInfo, parsedItem: ParsedInfo): ReceiptItem {
+    const index = items.findIndex(item => item.barcode === parsedItem.barcode)
+    const quantityInfo: QuantityInfo = { value: parsedItem.quantity, quantifier: items[index].unit + 's' }
+    const receiptItem: ReceiptItem = {
       name: '',
       quantity: quantityInfo,
       unitPrice: 0,
       subTotal: 0,
       discountTotal: 0
     }
-    receiptItem.name=items[index].name
-    receiptItem.unitPrice=items[index].price
-    receiptItem.discountTotal=this.caculateSubDiscount(parsedItem,this.isInPromotion(promotionInfo,parsedItem),receiptItem.unitPrice)
-    receiptItem.subTotal=this.caculateSubTotal(parsedItem,receiptItem.unitPrice,receiptItem.discountTotal)
+    receiptItem.name = items[index].name
+    receiptItem.unitPrice = items[index].price
+    receiptItem.discountTotal = this.caculateSubDiscount(parsedItem, this.isInPromotion(promotionInfo, parsedItem), receiptItem.unitPrice)
+    receiptItem.subTotal = this.caculateSubTotal(parsedItem, receiptItem.unitPrice, receiptItem.discountTotal)
     return receiptItem
   }
 
-  private isInPromotion(promotionInfo:PromotionInfo,parsedItem:ParsedInfo){
+  private isInPromotion(promotionInfo: PromotionInfo, parsedItem: ParsedInfo) {
     return promotionInfo.barcodes.includes(parsedItem.barcode)
   }
 
-  private caculateSubDiscount(parsedItem:ParsedInfo,promoted:boolean,unitPrice:number):number{
-    if(!promoted) return 0
-    return parsedItem.quantity/3*unitPrice
+  private caculateSubDiscount(parsedItem: ParsedInfo, promoted: boolean, unitPrice: number): number {
+    if (!promoted) return 0
+    return parsedItem.quantity / 3 * unitPrice
   }
 
-  private caculateSubTotal(parsedItem:ParsedInfo,unitPrice:number,subTotalDiscount:number){
-    return parsedItem.quantity*unitPrice-subTotalDiscount
+  private caculateSubTotal(parsedItem: ParsedInfo, unitPrice: number, subTotalDiscount: number) {
+    return parsedItem.quantity * unitPrice - subTotalDiscount
   }
 
-  private CaculateProductsTotalExpense()
+  private caculateProductsTotalExpense(receiptList: ReceiptItem[]): number {
+    let res = 0
+    for (let i = 0; i < receiptList.length; i++) {
+      res += receiptList[i].subTotal
+    }
+    return res
+  }
+
+  private caculateProductsTotalDiscount(receiptList: ReceiptItem[]): number {
+    let res = 0
+    for (let i = 0; i < receiptList.length; i++) {
+      res += receiptList[i].discountTotal
+    }
+    return res
+  }
+
+  private renderRecepit(totalExpense:number,totalDiscount:number,receiptList: ReceiptItem[]):string{
+    const title='***<store earning no money>Receipt ***'
+    const content = receiptList.map(item=>{
+      return `Name:${item.name},Quantity:${item.quantity.value} ${item.quantity.quantifier},Unit:${item.unitPrice.toFixed(2)},subTotal:${item.subTotal.toFixed(2)}(yuan)`
+    }).join('\n')
+    const dived='----------------------'
+    const conculusion = `Total:${totalExpense.toFixed(2)}(yuan)`+'\n'+`Total:${totalDiscount.toFixed(2)}(yuan)`
+    return title+content+dived+conculusion
+  }
 }
