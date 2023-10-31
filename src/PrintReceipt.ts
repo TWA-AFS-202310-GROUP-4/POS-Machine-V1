8a35a6416437485ad4263cba745762c41e2718fc
Xinwen Xiang <XXiang2@slb.com>
2023-10-31 21:00:16 +0800

    feat: add product tag validation and parse

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,12 +1,53 @@
-import {loadAllItems, loadPromotions} from './Dependencies'
+import { error } from 'console'
+import { loadAllItems } from './Dependencies'
+import { ItemInfo } from './model/itemInfo'
+import { ParsedInfo } from './model/parsedInfo'
 
-export function printReceipt(tags: string[]): string {
-  return `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`
+export class PrintReceipt {
+  public printReceipt(productList: string[]):string|null {
+
+    const parsedInfo = this.aggregateProductInfo(productList)
+    if (parsedInfo === null)
+      return null
+    return "ssss"
+  }
+
+  private isProductInfoValid(itemList: ItemInfo[], productionList: string[]): boolean {
+    const tagList = Array.from(itemList, item => item.barcode)
+    for (let i = 0; i < productionList.length; i++) {
+      if (tagList.indexOf(productionList[i].slice(0, 10)) === -1) {
+        return false
+      }
+    }
+    return true
+  }
+
+  private aggregateProductInfo(productList: string[]): ParsedInfo[] | null {
+    const parsedResult: ParsedInfo[] = []
+    const itemList = loadAllItems()
+    if (!this.isProductInfoValid(itemList, productList))
+      return null
+    productList.map(item =>
+      this.caculateProductQuantity(parsedResult, item)
+    )
+
+    return parsedResult
+
+  }
+
+  private caculateProductQuantity(currentParsedInfo: ParsedInfo[], productItem: string): ParsedInfo[] {
+    const index = currentParsedInfo.findIndex(item => item.barcode === productItem)
+    if (index === -1) {
+      currentParsedInfo.push({ barcode: productItem.slice(0, 10), quantity: this.parseProductQuantity(productItem) })
+    } else {
+      currentParsedInfo[index].quantity += this.parseProductQuantity(productItem)
+    }
+    return currentParsedInfo
+  }
+
+  private parseProductQuantity(productItem: string): number {
+    if (!productItem.includes('-'))
+      return 1
+    return Number(productItem.slice(productItem.indexOf('-') + 1))
+  }
 }
--- test/PrintReceipt.test.ts
+++ test/PrintReceipt.test.ts
@@ -1,7 +1,8 @@
-import {printReceipt} from '../src/PrintReceipt'
+import { PrintReceipt } from '../src/PrintReceipt'
 
-describe('printReceipt', () => {
-  it('should print receipt with promotion when print receipt', () => {
+describe('printReceipt validation test', () => {
+  it('should return null', () => {
+    const basic = new PrintReceipt()
     const tags = [
       'ITEM000001',
       'ITEM000001',
@@ -10,18 +11,28 @@ describe('printReceipt', () => {
       'ITEM000001',
       'ITEM000003-2.5',
       'ITEM000005',
+      'ITEM000011',
       'ITEM000005-2',
     ]
 
-    const expectText = `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`
-
-    expect(printReceipt(tags)).toEqual(expectText)
+    expect(basic.printReceipt(tags)).toBeNull()
   })
 })
+
+// describe('printReceipt', () => {
+//   it('should print parsed Info', () => {
+//     const basic = new PrintReceipt()
+//     const tags = [
+//       'ITEM000001',
+//       'ITEM000001',
+//       'ITEM000001',
+//       'ITEM000001',
+//       'ITEM000001',
+//       'ITEM000003-2.5',
+//       'ITEM000005',
+//       'ITEM000005-2',
+//     ]
+
+//     expect(basic.printReceipt(tags)).toEqual("{ barcode: 'ITEM000001', quantity: 5 },{ barcode: 'ITEM000003', quantity: 2.5 },{ barcode: 'ITEM000005', quantity: 1 },{ barcode: 'ITEM000005', quantity: 2 }")
+//   })
+// })
